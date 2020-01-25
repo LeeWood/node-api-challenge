9a56b5cd67378d66d4e024eb5c45f1ae7afedc38
lee-wood <leemaewood@gmail.com>
2020-01-24 21:33:03 -0500

    server base created and functional

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const projectsRouter = require('./data/helpers/projects/projectsRouter.js');
+const actionsRouter = require('./data/helpers/actions/actionsRouter.js');
+const server = express();
+const helmet = require('helmet');
+const cors = require('cors');
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+//server.use('/projects', projectsRouter);
+//server.use('/projects/:id/actions', actionsRouter); //!possibly alter this endpoint later.
+
+server.get('/', (req, res) => {
+  res.send(
+    `<h2>Sprint Challenge server up and running!</h2>`
+  )
+});
+
+module.exports = server;
