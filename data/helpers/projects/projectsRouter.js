19807630e6ea0e2cc3275e77b3657505e77b1ba9
lee-wood <leemaewood@gmail.com>
2020-01-24 21:41:16 -0500

    GET all projects endpoint functional

--- data/helpers/projects/projectsRouter.js
+++ data/helpers/projects/projectsRouter.js
@@ -3,6 +3,23 @@ const db = require('./projectModel.js');
 const router = express.Router();
 
 //GET REQUESTS
+router.get('/', (req, res) => {
+
+  db.get()
+    .then(projs => {
+      res.status(200).json({
+        success: true,
+        projs
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        message: "There has been a problem retrieving posts.",
+        error: err
+      });
+    });
+});
 
 //POST REQUESTS
 
